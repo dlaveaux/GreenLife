930ab50813bdaaed99c3c44a3702007360273788
dlaveaux <dlaveaux93@gmail.com>
2020-04-16 15:00:44 -0700

    ycomps

---
+++ comps/tips/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+import Logo from '../../Images/LOGO.png';
+import Happy from '../../Images/happy.png';
+
+
+export default function Tips(){
+    return (
+        // <h1>Based on your results...</h1>
+        // <h5>Food<h5>
+        // <h6>Eating homecooked meals<h6>
+        // <p>Saves Money</p>
+        // <p>Is Healthy</p>
+        // <p>Is Sustainable</p>
+        // <p>Is good for the planet</p>
+        // <p>Make you a better cook<p>
+        // <h5>Clothing<h5>
+        // <h6>Buying sustainable clothing<h6>
+        // <p>Less dependance on imported raw materials</p>
+        // <p>Helps promote jobs and industries that are eco-friendly</p>
+        // <p>Quality of clothing is usually better</p>
+        // <h5>Transportation<h5>
+        // <h6>Walking/Biking to work<h6>
+        // <p>Saves gas money</p>
+        // <p>Less pollution</p>
+        // <p>Is healthy</p>
+        // <p>Decreases stress<p>
+        
+        <img className ="happy" src = {Happy}></img>
+    );
+    
+}
+
+
+
