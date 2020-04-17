20d1ea5e61f64c8d9574e432827fbd9ecfeda9a1
dlaveaux <dlaveaux93@gmail.com>
2020-04-17 16:16:15 -0700

    did the header, results, and the tips button component,

--- comps/tips/index.js
+++ comps/tips/index.js
@@ -7,24 +7,30 @@ import Happy from '../../Images/happy.png';
 export default function Tips(){
     return (
         // <h1>Based on your results...</h1>
-        // <h5>Food<h5>
-        // <h6>Eating homecooked meals<h6>
-        // <p>Saves Money</p>
-        // <p>Is Healthy</p>
-        // <p>Is Sustainable</p>
-        // <p>Is good for the planet</p>
-        // <p>Make you a better cook<p>
-        // <h5>Clothing<h5>
-        // <h6>Buying sustainable clothing<h6>
-        // <p>Less dependance on imported raw materials</p>
-        // <p>Helps promote jobs and industries that are eco-friendly</p>
-        // <p>Quality of clothing is usually better</p>
-        // <h5>Transportation<h5>
-        // <h6>Walking/Biking to work<h6>
-        // <p>Saves gas money</p>
-        // <p>Less pollution</p>
-        // <p>Is healthy</p>
-        // <p>Decreases stress<p>
+        // <h5>Food</h5>
+        // <h6>Eating homecooked meals</h6>
+        // <ul>
+        //  <li>Saves Money</li>
+        // <li>Is Healthy</li>
+        // <li>Is Sustainable</li>
+        // <li>Is good for the planet</li>
+        // <li>Make you a better cook</li>
+        // </ul>
+        // <h5>Clothing</h5>
+        // <h6>Buying sustainable clothing</h6>
+        // <ul>
+        // <li>Less dependance on imported raw materials</li>
+        // <li>Helps promote jobs and industries that are eco-friendly</li>
+        // <li>Quality of clothing is usually better</li>
+        // </ul>
+        // <h5>Transportation</h5>
+        // <h6>Walking/Biking to work</h6>
+        // <ul>
+        // <li>Saves gas money</li>
+        // <li>Less pollution</li>
+        // <li>Is healthy</li>
+        // <li>Decreases stress</li>
+        // </ul>
         
         <img className ="happy" src = {Happy}></img>
     );
